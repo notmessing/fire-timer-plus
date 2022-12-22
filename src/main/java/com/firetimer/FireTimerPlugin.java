33fad85b75ea14f1bec3713cb52ad9287226ddda
autumn-smellegy <120493279+autumn-smellegy@users.noreply.github.com>
2022-12-22 16:28:37 -0500

    Update FireTimerPlugin.java

--- src/main/java/com/firetimer/FireTimerPlugin.java
+++ src/main/java/com/firetimer/FireTimerPlugin.java
@@ -2,13 +2,15 @@ package com.firetimer;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.AnimationChanged;
 import net.runelite.api.events.GameObjectSpawned;
+import net.runelite.api.events.GameObjectDespawned;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
@@ -16,9 +18,6 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
-
-import java.time.Instant;
-import java.util.ArrayList;
 
 @Slf4j
 @PluginDescriptor(
@@ -30,40 +29,29 @@ public class FireTimerPlugin extends Plugin
 	private Client client;
 
 	@Inject
-	private FireTimerConfig config;
-
-	@Inject
 	private OverlayManager overlayManager;
 
 	@Inject
 	private FireTimerOverlay fireTimerOverlay;
 
 	@Getter(AccessLevel.PACKAGE)
-	private Instant lastTickUpdate;
-
-	@Getter(AccessLevel.PACKAGE)
 	private long lastTrueTickUpdate;
 
 	@Getter(AccessLevel.PACKAGE)
-	private ArrayList<FireTimeLocation> fireIds;
-
-	private int counter = 0;
-
-	private boolean playerIsFiremaking = false;
+	private Map<Long, FireTimeLocation> fireIds;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		this.fireIds = new ArrayList<>(10);
-		this.overlayManager.add(fireTimerOverlay);
+		this.fireIds = new HashMap<>();
+		this.overlayManager.add(this.fireTimerOverlay);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
-		fireIds.clear();
-		overlayManager.remove(fireTimerOverlay);
+		this.fireIds.clear();
+		this.overlayManager.remove(this.fireTimerOverlay);
 	}
 
 	@Subscribe
@@ -72,48 +60,41 @@ public class FireTimerPlugin extends Plugin
 		if (event.getGameState() == GameState.LOGIN_SCREEN ||
 				event.getGameState() == GameState.HOPPING)
 		{
-			fireIds.clear();
+			this.fireIds.clear();
 		}
 	}
 
 	@Subscribe
-	public void onGameObjectSpawned(GameObjectSpawned objectSpawned)
-	{
-		if (objectSpawned.getGameObject().getId() == ObjectID.FIRE_26185 && this.playerIsFiremaking) {
-			log.info("Player fire spawned.");
-			this.playerIsFiremaking = false;
-			this.fireIds.add(
+	public void onGameObjectSpawned(GameObjectSpawned objectSpawned) {
+		if (objectSpawned.getGameObject().getId() == ObjectID.FIRE_26185) {
+			log.info("Fire spawned.");
+			this.fireIds.putIfAbsent(objectSpawned.getGameObject().getHash(),
 					new FireTimeLocation(
 							objectSpawned.getGameObject(),
 							objectSpawned.getGameObject().getWorldLocation(),
 							0,
-							Instant.now()
+							this.lastTrueTickUpdate
 					)
 			);
 		}
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick change) {
-		lastTrueTickUpdate = client.getTickCount();
-		lastTickUpdate = Instant.now();
-
-		for (FireTimeLocation fireTimeLocation : this.fireIds) {
-			fireTimeLocation.setTimeSinceFireLit(lastTickUpdate.getEpochSecond() - fireTimeLocation.getTickFireStarted().getEpochSecond());
+	public void onGameObjectDespawned(GameObjectDespawned objectDespawned) {
+		if (objectDespawned.getGameObject().getId() == ObjectID.FIRE_26185) {
+			log.info("Fire despawned.");
+			this.fireIds.remove(objectDespawned.getGameObject().getHash());
 		}
 	}
 
 	@Subscribe
-	public void onAnimationChanged(AnimationChanged change) {
-		Actor animationActor = change.getActor();
-		if (client.getLocalPlayer() != null) {
-			if (animationActor.getName() == client.getLocalPlayer().getName() && animationActor.getAnimation() == AnimationID.FIREMAKING) {
-				log.info("Firemaking animation started.");
-				this.playerIsFiremaking = true;
-			}
-		}
-	}
+	public void onGameTick(GameTick change) {
+		this.lastTrueTickUpdate = this.client.getTickCount();
 
+		this.fireIds.forEach((fireIdHash, fireTimeLocation) ->
+						fireTimeLocation.setTicksSinceFireLit(
+								this.lastTrueTickUpdate - fireTimeLocation.getTickFireStarted()));
+	}
 
 	@Provides
 	FireTimerConfig provideConfig(ConfigManager configManager)
